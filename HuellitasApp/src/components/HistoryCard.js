0535e606bd7738edac1c7466a3f639465b8fecc6
Frank5031 <20220332@ricaldone.edu.sv>
2024-06-28 19:25:41 -0600

    Historial a la mitad (ya se muestran los datos principales, solo falta el modal)

--- HuellitasApp/src/components/HistoryCard.js
+++ HuellitasApp/src/components/HistoryCard.js
@@ -1,102 +1,121 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
 import icon from '../../assets/historialCompra.png'; // Asegúrate de usar la ruta correcta
-
-const data = [
-  {
-    date: '13 de enero, 2024',
-    items: [
-      { title: 'Estado del pedido: Completado', quantity: 115, price: 40 },
-      { title: 'Bolsa pedigree - Perro mayor', quantity: 2, price: 40, unitPrice: 20 },
-    ],
-  },
-  {
-    date: '14 de enero, 2024',
-    items: [
-      { title: 'Bolsa pedigree - Perro mayor', quantity: 2, price: 40, unitPrice: 20 },
-      { title: 'Bolsa pedigree - Perro mayor', quantity: 2, price: 40, unitPrice: 20 },
-    ],
-  },
-  {
-    date: '15 de enero, 2024',
-    items: [
-      { title: 'Bolsa pedigree - Perro mayor', quantity: 2, price: 40, unitPrice: 20 },
-      { title: 'Bolsa pedigree - Perro mayor', quantity: 2, price: 40, unitPrice: 20 },
-    ],
-  },
-];
+import { useNavigation, useRoute, useFocusEffect } from "@react-navigation/native";
+import fetchData from '../../api/components';
 
 const HistoryCard = () => {
-  return (
-    <FlatList
-      data={data}
-      keyExtractor={(item) => item.date}
-      renderItem={({ item }) => (
-        <View style={styles.section}>
-          <Text style={styles.date}>{item.date}</Text>
-          {item.items.map((entry, index) => (
-            <View key={index} style={styles.card}>
-              <Image source={icon} style={styles.icon} />
-              <View style={styles.details}>
-                <Text style={styles.title}>{entry.title}</Text>
-                <Text style={styles.subtext}>
-                  Cantidad: {entry.quantity} Unidades{' '}
-                  {entry.unitPrice ? `Precio Individual: $${entry.unitPrice}.00` : ''}
-                </Text>
-              </View>
-              <Text style={styles.price}>${entry.price}.00</Text>
-            </View>
-          ))}
+    const [history, setHistory] = useState([]);
+
+    const fetchProfileData = async () => {
+        const USER_API = 'services/public/clientes.php';
+        try {
+            const response = await fetchData(USER_API, 'readHistorial');
+            console.log('Historial de compras:', response);
+
+            if (response && response.status === 1 && response.dataset) {
+                setHistory(response.dataset);
+            } else {
+                console.log('No se encontraron datos de historial de compras');
+            }
+        } catch (error) {
+            console.error('Error al obtener el historial de compras:', error);
+        }
+    };
+
+    useFocusEffect(
+        React.useCallback(() => {
+            fetchProfileData();
+        }, [])
+    );
+
+    const navigation = useNavigation();
+    const route = useRoute();
+    const { updated } = route.params || {};
+
+    useEffect(() => {
+        if (updated) {
+            fetchProfileData();
+        }
+    }, [updated]);
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={history}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.section}>
+                        <Text style={styles.date}>{item.fecha}</Text>
+                        <View style={styles.card}>
+                            <Image source={icon} style={styles.icon} />
+                            <View style={styles.details}>
+                                <Text style={styles.title}>{item.estado_pedido}</Text>
+                                <Text style={styles.subtext}>
+                                    Cantidad: {item.cantidad} Unidades
+                                </Text>
+                            </View>
+                            <Text style={styles.price}>${item.precio_total}</Text>
+                        </View>
+                        <View style={styles.separator} />
+                    </View>
+                )}
+            />
         </View>
-      )}
-    />
-  );
+    );
 };
 
 const styles = StyleSheet.create({
-  section: {
-    marginBottom: 16,
-  },
-  date: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 8,
-    paddingHorizontal: 16,
-  },
-  card: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 16,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    marginBottom: 8,
-    marginHorizontal: 16,
-  },
-  icon: {
-    width: 40,
-    height: 40,
-    marginRight: 12,
-  },
-  details: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  subtext: {
-    fontSize: 14,
-    color: '#666',
-  },
-  price: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+    },
+    date: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        paddingHorizontal: 16,
+        marginTop: 25,
+    },
+    card: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        marginBottom: 8,
+        marginHorizontal: 16,
+    },
+    icon: {
+        width: 40,
+        height: 40,
+        marginRight: 12,
+    },
+    details: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    subtext: {
+        fontSize: 14,
+        color: '#666',
+    },
+    price: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#e0e0e0',
+        marginHorizontal: 16,
+        marginTop: 8,
+    },
 });
 
 export default HistoryCard;
--- HuellitasApp/src/screens/History.js
+++ HuellitasApp/src/screens/History.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { View, StyleSheet, Image, Text, Dimensions, ScrollView } from "react-native";
 import PurchaseCard from '../components/HistoryCard';
 
 const History = () => {
   return (
-    <SafeAreaView>
-      <PurchaseCard />
-    </SafeAreaView>
+    <View style={styles.container}>
+        <PurchaseCard />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF'
+    }
+});
 
 export default History;
